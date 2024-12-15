b495d9fc048aa15e49056d857cd279513d65da28
speedking7 <235692975@qq.com>
2024-12-15 14:34:54 +0800

    Site updated: 2024-12-15 14:34:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/15/2024-12-15-测试文章/","2024/06/12/markdown/","2024/12/03/hello-world/"];function toRandomPost(){
+var posts=["2024/12/03/hello-world/","2024/06/12/markdown/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
